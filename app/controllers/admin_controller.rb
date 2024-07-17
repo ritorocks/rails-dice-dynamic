1d63032105b0e439278ba07694cf583dcf84b9bf
ritorocks <ritorocks@gmail.com>
2024-07-17 23:33:10 +0000

    Added logic for first 2 rolls under AdminController

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,20 +3,33 @@ class AdminController <ApplicationController
 
   end
 
-  def 2d6
+  def two_six
+    @rolls = []
+
+    2.times do
+      die = rand(1..6)
   
+      @rolls.push(die)
+    end  
     render({:template => "pages_templates/2d6"})
   end
 
-  def 2d10
+  def two_ten
+    @rolls = []
+
+    2.times do
+      die = rand(1..10)
+  
+      @rolls.push(die)
+    end  
     render({:template => "pages_templates/2d10"})
   end
 
-  def 1d20
+  def one_twenty
     render({:template => "pages_templates/1d20"})
   end
 
-  def 5d4
+  def five_four
     render({:template => "pages_templates/5d4"})
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
-  get("/", {: controller => "admin", :action => "show"})
-  get("/dice/2/6", {: controller => "admin", :action => "2d6"})
-  get("/dice/2/10", {: controller => "admin", :action => "2d10"})
-  get("/dice/1/20", {: controller => "admin", :action => "1d20"})
-  get("/dice/5/4", {: controller => "admin", :action => "5d4"})
+  get("/", {:controller => "admin", :action => "show"})
+  get("/dice/2/6", {:controller => "admin", :action => "two_six"})
+  get("/dice/2/10", {:controller => "admin", :action => "two_ten"})
+  get("/dice/1/20", {:controller => "admin", :action => "one_twenty"})
+  get("/dice/5/4", {:controller => "admin", :action => "five_four"})
 
 end
